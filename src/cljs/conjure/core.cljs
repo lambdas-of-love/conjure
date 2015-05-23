f05601e098bcd0578710bba4084084ba2d54b2a9
Brian Dawn <brian.t.dawn@gmail.com>
2015-05-23 14:34:06 -0500

    Added client side websocket configuration.

--- src/cljs/conjure/core.cljs
+++ src/cljs/conjure/core.cljs
@@ -1,6 +1,29 @@
 (ns ^:figwheel-always conjure.core
+    (:require-macros
+     [cljs.core.async.macros :as asyncm :refer (go go-loop)])
     (:require
-              [reagent.core :as reagent :refer [atom]]))
+     [reagent.core :as reagent :refer [atom]]
+     [cljs.core.async :as async :refer (<! >! put! chan)]
+     [taoensso.sente  :as sente :refer (cb-success?)]))
+
+
+;; Sente initialization.
+(defn chsk-url-fn
+  "This is how we tell Sente where our server is lolz."
+  [path {:as window-location :keys [protocol pathname]} websocket?]
+  (let [my-host "127.0.0.1:8080"]
+    (str (if-not websocket? protocol (if (= protocol "https:") "wss:" "ws:"))
+      "//" my-host (or path pathname))))
+
+(let [{:keys [chsk ch-recv send-fn state]}
+      (sente/make-channel-socket! "/chsk" ; Note the same path as before
+       {:type :auto ; e/o #{:auto :ajax :ws}
+        :chsk-url-fn chsk-url-fn})]
+  (def chsk       chsk)
+  (def ch-chsk    ch-recv) ; ChannelSocket's receive channel
+  (def chsk-send! send-fn) ; ChannelSocket's send API fn
+  (def chsk-state state)   ; Watchable, read-only atom
+  )
 
 (enable-console-print!)
 
